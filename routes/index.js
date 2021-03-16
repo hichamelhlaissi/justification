ee9d92d662c2de932e7ad8d0d681ae22f96d8a41
Hicham Elhlaissi <elhlaissihicham@gmail.com>
2021-03-16 05:54:30 +0100

    fix bug

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'justification - /api/signIn for sign in, and /api/gettext to test the app' });
 });
 
 module.exports = router;
