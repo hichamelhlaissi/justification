cb2e73a5adc88999ed84f20b04d153d3e191ce02
Hicham Elhlaissi <elhlaissihicham@gmail.com>
2021-03-16 18:07:33 +0100

    fix bug

--- app.js
+++ app.js
@@ -14,15 +14,19 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors);
 app.use('/', indexRouter);
 app.use("/api/gettext", textCheckRouter,);
 app.use('/api/signin', signInRouter);
+app.use('/test', (req, res) => {
+  console.log('hahahaha')
+  res.end()
+})
 
 
 // catch 404 and forward to error handler
--- routes/signIn.js
+++ routes/signIn.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const signIn = require('../src/controllers/signIn.controller')
 
-router.post('', signIn.signIn_post);
+router.get('', signIn.signIn_post);
 
 module.exports = router;
