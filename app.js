9504e0a13f1edfd08a9c2ac7119d41a2bca65963
Hicham Elhlaissi <elhlaissihicham@gmail.com>
2021-03-16 06:31:21 +0100

    fix bug

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors);
 app.use('/', indexRouter);
 app.use("/api/gettext", textCheckRouter,);
 app.use('/api/signin', signInRouter);
