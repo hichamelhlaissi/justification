8a426ec3627a90f55f8db9d41eb9697bf2341cfa
Hicham Elhlaissi <elhlaissihicham@gmail.com>
2021-03-16 06:37:46 +0100

    fix bug

--- routes/signIn.js
+++ routes/signIn.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const signIn = require('../src/controllers/signIn.controller')
 
-router.get('', signIn.signIn_post);
+router.post('', signIn.signIn_post);
 
 module.exports = router;
